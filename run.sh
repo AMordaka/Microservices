fc0a5490d86a1916cfabe950b442ab75b2403e92
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2019-10-26 21:56:36 +0200

    Dockerization - done

--- run.sh
+++ run.sh
@@ -1,3 +1,27 @@
-mvn clean install
-docker-compose down -v --rmi all --remove-orphans
-docker-compose up
+#!/bin/bash
+
+eurka_uri_env="http://172.17.0.3:8761/eureka"
+config_uri="http://172.17.0.2:8888"
+db_uri="jdbc:h2:tcp://172.17.0.5:9090/mem:testdb"
+
+echo "ENV variables:"
+echo "EUREKA URI: $eurka_uri_env"
+echo "CONFIG URI: $config_uri"
+echo "DB URI: $db_uri"
+
+echo "Config Server starting..."
+docker run -d -it --name config-server-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri config-server
+sleep 15
+echo "Registry Service starting..."
+docker run -d -it --name registry-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri registry-service
+sleep 15
+echo "Gateway Service starting..."
+docker run -d -it --name gateway-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri gateway-service
+sleep 15
+echo "Other Services starting..."
+docker run -d -it --name auth-center-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri auth-center
+docker run -d -it --name admin-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri -e DB=$db_uri admin-service
+docker run -d -it --name occupant-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri -e DB=$db_uri occupant-service
+docker run -d -it --name manager-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri -e DB=$db_uri manager-service
+echo "DONE"
+
