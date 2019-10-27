6aad5db93e52c1e9311a95413ccd7d8e8eba11d7
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2019-10-27 21:54:13 +0100

    Add env

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@
 eurka_uri_env="http://172.17.0.4:8761/eureka"
 config_uri="http://172.17.0.3:8888"
 db_uri="jdbc:postgresql://172.17.0.2:5432/housingdb"
+auth_center_uri="http://172.17.0.6:8085"
+admin_service_uri="http://172.17.0.7:8092"
+occupant_service_uri="http://172.17.0.8:8091"
+manager_service_uri="http://172.17.0.9:8090"
 
 echo "ENV variables:"
 echo "EUREKA URI: $eurka_uri_env"
@@ -20,7 +24,7 @@ echo "Registry Service starting..."
 docker run -d -it --name registry-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri registry-service
 sleep 15
 echo "Gateway Service starting..."
-docker run -d -it --name gateway-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri gateway-service
+docker run -d -it --name gateway-service-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri -e AUTH_CENTER_URL=$auth_center_uri -e MANGER_SERVICE_URL=$manager_service_uri -e OCCUPANT_SERVICE_URL=$occupant_service_uri -e ADMIN_SERVICE_URL=$admin_service_uri gateway-service
 sleep 15
 echo "Other Services starting..."
 docker run -d -it --name auth-center-cr -e EUREKA_URI=$eurka_uri_env -e URI=$config_uri -e DB=$db_uri auth-center
